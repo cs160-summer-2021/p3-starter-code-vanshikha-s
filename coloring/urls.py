92bb6eb8dbfc09b7ae38fcff0949ae7ffd5aa41c
Vanshikha Sinha <vanshikha.sinha@berkeley.edu>
2021-07-12 20:57:21 -0400

    vanshikha's first push

--- coloring/urls.py
+++ coloring/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('demo/', views.demo, name='demo'),
+    path('index/', views.index, name='index'),
+    path('info/', views.info, name='info'),
+    path('new/interaction/', views.new_interaction, name='new_interaction'),
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def demo(request):
+    return render(request, 'coloring/demo.html')
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def info(request):
+    return render(request, 'coloring/info.html')
+
+def new_interaction(request):
+    return render(request, 'coloring/comparison-alert.html')
