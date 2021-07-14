68fa75480a489761f66d0e8db4c2492b166161fd
Vanshikha Sinha <vanshikha.sinha@berkeley.edu>
2021-07-13 20:28:10 -0400

    vanshikha's push2.0

--- coloring/views.py
+++ coloring/views.py
@@ -4,20 +4,20 @@ from django.http import HttpResponse
 def demo(request):
     return render(request, 'coloring/demo.html')
 
+
 def index(request):
     return render(request, 'coloring/index.html')
 
-<<<<<<< HEAD
+
 def info(request):
     return render(request, 'coloring/info.html')
 
+
 def new_interaction(request):
-    return render(request, 'coloring/comparison-alert.html')
+    return render(request, 'coloring/new_interaction.html')
 
 def template_options(request):
     return render(request, 'coloring/template_options.html')
 
-=======
-def template_options(request):
-    return render(request, 'coloring/template_options.html')
->>>>>>> origin
+def do_not_disturb(request):
+    return render(request, 'coloring/do_not_disturb.html')
