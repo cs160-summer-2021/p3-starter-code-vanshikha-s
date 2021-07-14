5abf1a76d28a15c709996e5753461265270e5661
Sakshi Satpathy <sakshi.satpathy@berkeley.edu>
2021-07-14 16:44:05 -0700

    Local

--- coloring/urls.py
+++ coloring/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction'),
-    path('template_options', views.template_options, name='template_options')
+    path('demo/', views.demo, name='demo'),
+    path('index/', views.index, name='index'),
+    path('info/', views.info, name='info'),
+    path('new/interaction/one/', views.new_interaction_one, name='new_interaction_one'),
+    path('new/interaction/two/', views.new_interaction_two, name='new_interaction_two'),
+    path('template/options/', views.template_options, name='template_options'),
+    path('do/not/disturb/', views.do_not_disturb, name='do_not_disturb'),
+    path('done/', views.done, name='done'),
+    path('exit/message/', views.exit_message, name='exit_message'),
+    path('save/reminder/one', views.save_reminder_one, name='save_reminder_one'),
+    path('save/reminder/two', views.save_reminder_two, name='save_reminder_two'),
+   
+
+   
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def demo(request):
+    return render(request, 'coloring/demo.html')
+
 
 def index(request):
     return render(request, 'coloring/index.html')
 
+
+def info(request):
+    return render(request, 'coloring/info.html')
+
+
+def new_interaction_one(request):
+    return render(request, 'coloring/new_interaction_one.html')
+
+def new_interaction_two(request):
+    return render(request, 'coloring/new_interaction_two.html')
+
 def template_options(request):
     return render(request, 'coloring/template_options.html')
+
+def do_not_disturb(request):
+    return render(request, 'coloring/do_not_disturb.html')
+
+def done(request):
+    return render(request, 'coloring/done.html')
+
+def exit_message(request):
+    return render(request, 'coloring/exit_message.html')
+
+def save_reminder_one(request):
+    return render(request, 'coloring/save_reminder_one.html')
+
+def save_reminder_two(request):
+    return render(request, 'coloring/save_reminder_two.html')
